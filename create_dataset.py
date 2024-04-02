190d11c5bd90bfcff7625c1718bbe353a2092318
michaeldisanto1818 <156462477+michaeldisanto1818@users.noreply.github.com>
2024-04-02 15:59:54 -0400

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -10,9 +10,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(
     static_image_mode=True,
-    min_detection_confidence=0.3,
-    min_tracking_confidence=0.5,
-    max_num_hands=2)
+    min_detection_confidence=0.3)
 
 DATA_DIR = './data'
 
